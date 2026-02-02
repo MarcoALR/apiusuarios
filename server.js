acecf1892d1ba9fcfcf1326a8b8a62c37f19ae90
Marco A. <161896294+MarcoALR@users.noreply.github.com>
2026-02-01 21:44:07 -0300

    🎈

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "https://agenda-pj.vercel.app",
-    "http://localhost:5173/",
-    "http://localhost:5173/"
+    "http://localhost:5173"
   ],
   credentials: true,
 }));
